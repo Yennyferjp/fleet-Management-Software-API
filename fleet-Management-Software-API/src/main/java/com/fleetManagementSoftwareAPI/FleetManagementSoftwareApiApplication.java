04d74431e6a948219c00cf3403e1da08604c49f1
Yennyferjp <yennyfer.jarava92@gmail.com>
2024-02-06 14:32:36 -0500

    Initial commit

---
+++ fleet-Management-Software-API/src/main/java/com/fleetManagementSoftwareAPI/FleetManagementSoftwareApiApplication.java
@@ -0,0 +1,19 @@
+package com.fleetManagementSoftwareAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FleetManagementSoftwareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementSoftwareApiApplication.class, args);
+	}
+
+
+}
+
+
+
+
---
+++ fleet-Management-Software-API/src/test/java/com/fleetManagementSoftwareAPI/FleetManagementSoftwareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetManagementSoftwareAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementSoftwareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
