f0d641823129a297402a96878636e073e51bac9a
Yennyferjp <yennyfer.jarava92@gmail.com>
2024-02-06 20:49:03 -0500

    Endpoint listado de taxis

--- fleet-Management-Software-API/src/main/java/com/fleetManagementSoftwareAPI/taxis/TaxisController.java
+++ fleet-Management-Software-API/src/main/java/com/fleetManagementSoftwareAPI/taxis/TaxisController.java
@@ -1,8 +1,10 @@
 package com.fleetManagementSoftwareAPI.taxis;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/taxis")
 public class TaxisController {
+
 
     public final TaxisService taxisService;
 
@@ -19,8 +22,9 @@ public class TaxisController {
     }
 
     @GetMapping()
-    public List<Taxis> getTaxis(){
-        return taxisService.getTaxis();
+    public Page<Taxis> getTaxis(@RequestParam(defaultValue = "0") int pageNumber,
+                                @RequestParam(defaultValue = "10") int pageSize){
+        return taxisService.getTaxis(pageNumber, pageSize);
     }
 
 }
