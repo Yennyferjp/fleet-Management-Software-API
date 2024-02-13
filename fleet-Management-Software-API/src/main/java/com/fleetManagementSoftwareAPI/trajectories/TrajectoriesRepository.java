dabea1a0d8df26c3dff8e06296b2c83980cffdf0
Yennyferjp <yennyfer.jarava92@gmail.com>
2024-02-13 07:09:08 -0500

    OrderByDateDesc()

--- fleet-Management-Software-API/src/main/java/com/fleetManagementSoftwareAPI/trajectories/TrajectoriesRepository.java
+++ fleet-Management-Software-API/src/main/java/com/fleetManagementSoftwareAPI/trajectories/TrajectoriesRepository.java
@@ -4,12 +4,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public interface TrajectoriesRepository extends JpaRepository<Trajectories, Long> {
+    //@Query("SELECT t FROM Trajectories t WHERE t.taxi_id = ?1 AND t.date <= ?2 ORDER BY t.date DESC")
 
-    Page<Trajectories> findByTaxiIdAndDateGreaterThanEqualAndDateBefore(
+    Page<Trajectories> findByTaxiIdAndDateGreaterThanEqualAndDateBeforeOrderByDateDesc(
             Long taxiId, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable
     );
 // >= 2008-02-02 AND date < 2008-02-03
